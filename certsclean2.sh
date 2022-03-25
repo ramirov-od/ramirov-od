29ce4a57893d189375c22c580c1da39fef6f37af
Ruslan Amirov <HQ\ramirov@ramirov-admin.hq.overdrive.com>
2022-03-25 15:22:11 +0000

    Update of the Script

--- certsclean2.sh
+++ certsclean2.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 #set -x 
-echo "Displaying today's date
------------------------------"
+echo ""
+echo "--> Displaying todays date"
+echo ""
 sleep 2
+
 DATE=$(date '+%Y-%m-%dT%H:%M:%S+00:00')
              
+echo "--> Todays date is ----- "$DATE" ------ "
+echo ""
+echo "--> Collecting certs IDs and parsing EXPIRED ones into json file."
+echo "-----------------------------------------------------------------"
 
-echo 'Today's date is ----- $DATE ------
+for c in $(aws acm list-certificates --query 'CertificateSummaryList[].CertificateArn' --output text); do 
+    aws acm describe-certificate  --certificate-arn "$c"  --output json | jq --arg date "$DATE" -r '.| select(.Certificate.NotAfter <= $date ) | .Certificate.CertificateArn' >> certs2.json
+    echo "Processing --> $c"
+    #Looping through each line of certs2.json to collect arn of each cert and then deleting it
+done
 
-echo '
-Collecting certs IDs and parsing them into json file.
------------------------------------------------------------'
-for c in $(aws acm list-certificates --query 'CertificateSummaryList[].CertificateArn' --output text)
-do aws acm describe-certificate  --certificate-arn $c  --output json | jq --arg date $DATE -r '.| select(.Certificate.NotAfter <= $date ) | .Certificate.CertificateArn' >> certs2.json
-echo "Processing $c"
-#Looping through each line of certs2.json to collect arn of each cert and then deleting it
-done 
-while read line; do aws acm delete-certificate --certificate-arn $line --output text
+while read -r line; do 
+    aws acm delete-certificate --certificate-arn "$line" --output text
+    echo "Deleting Expired Certificate --> "$line" "
 done <certs2.json
 
 #echo "Deleting certs2.json File"
